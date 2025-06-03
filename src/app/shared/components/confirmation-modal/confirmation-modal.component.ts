7c2f1a48ef3ff3eae3c71628093c06be4552cf8b
jpablopachar <jpablopachar1993@gmail.com>
2025-06-02 21:13:50 -0500

    Eliminar propiedades innecesarias de los componentes de modal de confirmación y esqueleto de carga

--- src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,11 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-confirmation-modal',
-  standalone: true,
-  imports: [],
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConfirmationModalComponent { }
--- src/app/shared/components/loading-skeleton/loading-skeleton.component.ts
+++ src/app/shared/components/loading-skeleton/loading-skeleton.component.ts
@@ -1,11 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-loading-skeleton',
-  standalone: true,
-  imports: [],
   templateUrl: './loading-skeleton.component.html',
   styleUrls: ['./loading-skeleton.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoadingSkeletonComponent { }
